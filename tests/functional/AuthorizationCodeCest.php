ca924143fe38398be4960c1a942236540ca4f92a
Philippe Gaultier <pgaultier@ibitux.com>
2016-11-06 18:57:06 +0100

    Move back to yii2-redis 2.0.4 because tests cannot run with 2.0.5

--- tests/functional/AuthorizationCodeCest.php
+++ tests/functional/AuthorizationCodeCest.php
@@ -15,13 +15,30 @@
 namespace tests\functional;
 
 use FunctionalTester;
+use sweelix\oauth2\server\models\AccessToken;
+use sweelix\oauth2\server\models\RefreshToken;
+use sweelix\oauth2\server\models\AuthCode;
 use Yii;
+use yii\helpers\Json;
 
 class AuthorizeCest extends CestCase
 {
     public function _before(FunctionalTester $I)
     {
         $this->cleanDatabase();
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+        // Yii::$app->redis->close();
+        // $this->destroyApplication();
+    }
+
+    public function checkBadAccess(FunctionalTester $I)
+    {
+        $I->amOnRoute('oauth2/authorize/login');
+        $I->see('Bad Request', 'h1');
+        $I->see('request_invalid', 'h4');
     }
 
     public function checkAuthorizeWithBadClient(FunctionalTester $I)
@@ -35,6 +52,27 @@ class AuthorizeCest extends CestCase
         $I->see('invalid_client', 'h4');
     }
 
+    public function checkAuthorizeWithCorrectClientInvalidGrant(FunctionalTester $I)
+    {
+        $client = Yii::createObject('sweelix\oauth2\server\interfaces\ClientModelInterface');
+        /* @var \sweelix\oauth2\server\interfaces\ClientModelInterface $client */
+        $client->id = 'client2';
+        $client->secret = 'secret2';
+        $client->isPublic = false;
+        $client->userId = 'uid';
+        $client->name = 'Test client 2';
+        $I->assertTrue($client->save());
+
+        Yii::$app->getModule('oauth2')->allowAuthorizationCode = false;
+
+        $I->amOnRoute('oauth2/authorize/index', [
+            'response_type' => 'code',
+            'client_id' => 'client2',
+            'redirect_uri' => 'http://localhost/cb'
+        ]);
+        $I->see('Bad Request', 'h1');
+        $I->see('invalid_grant', 'h4');
+    }
     public function checkAuthorizeWithCorrectClientAndDecline(FunctionalTester $I)
     {
         $client = Yii::createObject('sweelix\oauth2\server\interfaces\ClientModelInterface');
@@ -103,8 +141,29 @@ class AuthorizeCest extends CestCase
 
         $I->click('AUTHORIZE');
 
-        // $I->seeInCurrentUrl('code=');
-        // $code = $I->grabFromCurrentUrl('~code=([^&]+)~');
+        $I->seeInCurrentUrl('code=');
+        $code = $I->grabFromCurrentUrl('~code=([^&]+)~');
+
+        $codeModel = AuthCode::findOne($code);
+        $I->assertInstanceOf(AuthCode::className(), $codeModel);
+        $I->assertEquals('userid1', $codeModel->userId);
+        $response = $I->requestRoute('POST', 'oauth2/token/index', [], [
+            'client_id' => 'client2',
+            'client_secret' => 'secret2',
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => 'http://localhost/cb',
+            'code' => $codeModel->id,
+        ]);
+        $response = Json::decode($response);
+        $codeModel = AuthCode::findOne($code);
+        $I->assertNull($codeModel);
+        $accessToken = AccessToken::findOne($response['access_token']);
+        $I->assertInstanceOf(AccessToken::className(), $accessToken);
+        $refreshToken = RefreshToken::findOne($response['refresh_token']);
+        $I->assertInstanceOf(RefreshToken::className(), $refreshToken);
+
+        // {"access_token":"6707b5bebebd0849e8ce47573a26b66a899f7cd3","expires_in":3600,"token_type":"Bearer","scope":null,"refresh_token":"be7f02247added9f47aad257a8bb24dcc74072d5"}
+
         // die($code);
     }
 }
