f1386d39990ac7b17ac0ca0c00967734233ae3da
Philippe Gaultier <pgaultier@ibitux.com>
2016-12-06 16:58:11 +0100

    Fix vendor Path and assets Path to allow testing

--- tests/config/test.php
+++ tests/config/test.php
@@ -13,6 +13,7 @@
  */
 return [
     'basePath' => __DIR__ . '/../../src',
+    // Force vendor path to avoid misconfiguration
     'vendorPath' => __DIR__.'/../../vendor',
     'id' => 'sweelix/yii2-oauth2-server-testing',
     'timeZone' => 'Europe/Paris',
@@ -36,6 +37,11 @@ return [
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
             ],
+        ],
+        // Force assets publish directory to avoid adding useless directories
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'basePath' => '@tests/_output/assets'
         ],
         'user' => [
             'class' => 'sweelix\oauth2\server\web\User',
